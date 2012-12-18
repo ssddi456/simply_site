adaaac9ddb60689f036e815052d3aa144a94090d
torrent88410@gmail.com <torrent88410@gmail.com>
2012-12-18 23:25:37 +0800

    send mail function
    functional

---
+++ sendmail.php
@@ -0,0 +1,41 @@
+<?php
+$STMP_HOST = 'localhost';
+$STMP_PORT = 25;
+
+$AUTOMAIL = 
+'name      : %s
+company   : %s
+telephone : %s
+email     : %s
+
+%s';
+
+ini_set('SMTP',      $STMP_HOST );
+ini_set('smtp_port', $SMTP_PORT );
+
+$to = 'lihaibo@cdsf.com';
+$subject = '[auto mail] from your site, some one is interested in your courses';
+
+$headers = 'MIME-Version: 1.0\r\n';
+$headers .= 'MIME-Version: 1.0\r\n';
+$headers .= 'Content-type:text/plain;charset=utf-8\r\n';
+
+// 更多报头
+
+$headers .= 'From: <automail@sina.com>\r\n';
+$headers .= 'Cc: automail@sina.com\r\n';
+
+$message = sprintf($AUTOMAIL,
+  $_POST['name'],
+  $_POST['company'],
+  $_POST['telephone'],
+  $_POST['email'],
+  $_POST['content']);
+
+$ret = mail($to,$subject,$message,$headers);
+if($ret){
+  echo('an email has been send');
+}else{
+  echo('there is some problem during mail sending');
+}
+?>
